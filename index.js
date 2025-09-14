969e2392545f8798fcdadf77642ee6ae8fcfb6fc
Bashir <bashir6412@gmail.com>
2025-09-14 18:33:26 +0400

    Alot of coding perfore intialzing git

---
+++ index.js
@@ -0,0 +1,18 @@
+let tasks = [];
+
+count = Taskinput = document.getElementById("inputb");
+count = AddTask = document.getElementById("addtask");
+count = TaskList = document.getElementById("TaskList")
+
+AddTask.addEventListener('click', function() {
+    const task = taskinput.value.trim();
+    if (task !== "") {
+        tasks.push(task);
+        renderTasks();
+        taskinput.value = "";
+    }
+})
+
+function renderTasks() {
+    
+}
